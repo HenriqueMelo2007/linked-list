1d34eb462c93771f488e05c6cf10884372eb66c8
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-07-04 10:27:37 -0300

    feat: it adds interative text

--- main.c
+++ main.c
@@ -15,7 +15,24 @@ int main(void)
 
   if ( list == NULL ) return 1;
 
-  printf("List initialized with: %i", list->num);
+  printf("List initialized with: %i\n", list->num);
+
+  int userInput;
+  do {
+    printf("Type:\n");
+    printf("1 - to insert at the beggining\n");
+    printf("2 - to insert at a specific position\n");
+    printf("3 - to insert at the end\n");
+    printf("4 - to remove at the beggining\n");
+    printf("5 - to remove at a specific position\n");
+    printf("6 - to remove at the end\n");
+    printf("7 - to count elements of the list\n");
+    printf("8 - to print elements of the list\n");
+    printf("9 - to leave the program\n");
+
+
+    scanf("%i", &userInput);
+  } while ( userInput != 9 );
 
   return 0;
 }
@@ -33,4 +50,6 @@ void initializeList (Node** list) {
 
   *list = newNode;
 }
+
+void append (Node* list) {}
 
